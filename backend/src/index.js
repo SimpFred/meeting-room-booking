b23e04f8c853d5fe831dfdf9510c87cf9c6f26bd
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-11-06 17:10:02 +0100

    Add cors package for handling cross-origin resource sharing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import roomRoutes from './routes/roomRoutes.js';
 import bookingRoutes from './routes/bookingRoutes.js';
 
 const app = express();
+
+// CORS configuration
+const corsOptions = {
+    origin: 'http://localhost:3000'
+  };
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/rooms', roomRoutes);
