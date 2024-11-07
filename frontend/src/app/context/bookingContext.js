44c41e78582087e2793c9a830e12ddd45419fa06
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-11-07 16:34:49 +0100

    Add fetchRooms and fetchBookings functions to API client and some refactoring

--- frontend/src/app/context/bookingContext.js
+++ frontend/src/app/context/bookingContext.js
@@ -1,23 +1,31 @@
 // BookingContext.js
 import React, { createContext, useState, useEffect, useRef } from 'react';
+import { fetchBookings, fetchRooms } from '../service/apiClient';
 
 const BookingContext = createContext();
 
 export const BookingProvider = ({ children }) => {
   const [rooms, setRooms] = useState([]);
+  const [bookings, setBookings] = useState([]);
   const [checkedRooms, setCheckedRooms] = useState([]);
   const [selectedRooms, setSelectedRooms] = useState([]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedRoom, setSelectedRoom] = useState({ roomId: null, hour: null, date: null });
   const dropdownRef = useRef(null);
 
+  const refreshData = async () => {
+    const roomsData = await fetchRooms();
+    setRooms(roomsData);
+
+    const bookingsData = await fetchBookings();
+    setBookings(bookingsData);
+    console.log('Bookings: ', bookingsData);
+  };
+
   useEffect(() => {
-    async function fetchRooms() {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/rooms`);
-      const data = await res.json();
-      setRooms(data);
-    }
-    fetchRooms();
+    refreshData();
   }, []);
+
 
   const handleRoomSelection = (roomName) => {
     setCheckedRooms((prevSelected) =>
@@ -43,14 +51,18 @@ export const BookingProvider = ({ children }) => {
     <BookingContext.Provider
       value={{
         rooms,
+        bookings,
         checkedRooms,
         selectedRooms,
         isDropdownOpen,
         dropdownRef,
+        selectedRoom,
+        setSelectedRoom,
         handleRoomSelection,
         toggleDropdown,
         handleConfirmSelection,
         handleClearSelection,
+        refreshData,
       }}
     >
       {children}
