6b4ad61e9e78c675aac598e8f1297dd5648c4ed4
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-11-07 16:32:53 +0100

    feat: Add Title component for displaying a heading text

---
+++ frontend/src/app/components/title.js
@@ -0,0 +1,9 @@
+const Title = ({ text }) => {
+    return (
+      <h1 className="font-normal mt-[80px] text-[40px] leading-[100%] text-black">
+        {text}
+      </h1>
+    );
+  };
+  
+  export default Title;
