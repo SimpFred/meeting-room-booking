54165719defdce46c14a49259baf63f014a9cf10
Simon Fredriksson <simon.fredriksson93@gmail.com>
2024-11-08 14:25:18 +0100

    add warning message
    A warning message is displayed when a room is not selected before proceeding.

--- frontend/src/app/pages/booking/page.js
+++ frontend/src/app/pages/booking/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import DropdownContainer from '../../components/dropDownMenu/dropDownContainer';
 import BookingCalendar from '../../components/bookingCalandar/bookingCalandar';
@@ -11,25 +11,36 @@ import Title from '../../components/title';
 export default function Booking() {
   const { selectedRoom } = useContext(BookingContext);
   const router = useRouter();
+  const [showWarning, setShowWarning] = useState(false);
   const isNextButtonDisabled = !selectedRoom.roomId || !selectedRoom.hour || !selectedRoom.date;
+
+  useEffect(() => {
+    if (showWarning) {
+        setShowWarning(false);
+    }
+  }, [selectedRoom]);
 
   const handleNextClick = () => {
     if (!isNextButtonDisabled) {
       router.push('/pages/confirmation');
+    } else {
+      setShowWarning(true);
     }
   };
 
-
   return (
-      <div className="max-h-screen p-6 mb-[53px]">
-        <Title text="Välj en tid" />
-        <DropdownContainer />
-        <div className="mt-[40px]" style={{ maxHeight: '55vh', overflowY: 'auto' }}>
-          <BookingCalendar />
-        </div>
-        <div className="mt-[27px]">
-          <Button text="nästa" onClick={handleNextClick} disabled={isNextButtonDisabled} />
-        </div>
+    <div className="max-h-screen p-6 mb-[53px]">
+      <Title text="Välj en tid" />
+      <DropdownContainer />
+      <div className="mt-[40px]" style={{ maxHeight: '55vh', overflowY: 'auto' }}>
+        <BookingCalendar />
       </div>
+      <div className="mt-[27px]">
+        <Button text="nästa" onClick={handleNextClick} disabled={isNextButtonDisabled} />
+        {showWarning && (
+          <p className="text-red-500 text-center mt-2">Ett rum måste vara markerat för att gå vidare</p>
+        )}
+      </div>
+    </div>
   );
 }
