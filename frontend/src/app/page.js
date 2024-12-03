1546504a78b37e10fb740e38c8260a5a619a7069
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-12-03 12:47:32 +0100

    refactor: restructure layout in Home component for improved organization and readability

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -11,15 +11,15 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen p-6 pb-[53px] flex flex-col justify-between">
-      <main className="flex flex-col gap-8 items-center">
+    <main className="min-h-screen p-6 pb-[53px] flex flex-col justify-between">
+      <div className="flex flex-col gap-8 items-center">
         <h1 className="font-normal text-[80px] leading-[80px] align-cap mt-[124px]">
           Boka ett rum
         </h1>
-      </main>
-      <footer className="flex justify-center">
+      </div>
+      <div className="flex justify-center">
         <Button text="Boka" onClick={handleBookClick} />
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
