315cab0836e49f5621143fedfe76b4e88e40c106
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-12-04 12:46:17 +0100

    refactor: improve code formatting and indentation in BookingCalendar component

--- frontend/src/app/components/bookingCalandar/bookingCalandar.js
+++ frontend/src/app/components/bookingCalandar/bookingCalandar.js
@@ -4,23 +4,20 @@ import { useContext } from "react";
 import BookingContext from "../../context/bookingContext";
 
 const BookingCalendar = () => {
-    const { dates } = useContext(BookingContext);
+  const { dates } = useContext(BookingContext);
 
-    return (
-        <div className="mb-[27px]">
-            <Navigation />
-            <div className="border rounded-lg mt-[24px] border border-gray-500">
-                <div className="grid grid-cols-3">
-                    {dates.map((date) => (
-                        <DateColumn
-                            key={date}
-                            date={date}
-                        />
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className="mb-[27px]">
+      <Navigation />
+      <div className="border rounded-lg mt-[24px] border border-gray-500">
+        <div className="grid grid-cols-3">
+          {dates.map((date) => (
+            <DateColumn key={date} date={date} />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BookingCalendar;
